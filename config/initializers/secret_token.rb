33e8833bf5e440e68548c5ba18925985100b8c47
Jim Knight <jim.knight@lavatech.com>
2013-01-19 19:26:13 -0500

    initial commit for trevorland

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trevorland::Application.config.secret_token = 'dc81178418046c7440e9d15f4dc769fbfca71eb3a875f615ee07d256eb3528fe203ef6dfc0f4a4c5ebcf670c46553ca69357d4d32fbae983562094d2ac8ef17f'
